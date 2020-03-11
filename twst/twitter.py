e0a9e0ece1fcab9b777a72630bf70dc09e1c2582
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-03-11 08:52:54 +0100

    Add compatibility for extended tweets

--- twst/twitter.py
+++ twst/twitter.py
@@ -1,10 +1,9 @@
 import tweepy
-from dotenv import load_dotenv
 import os
-load_dotenv()
 
-consumer_key = os.environ['API_KEY']
-consumer_secret = os.environ['API_SECRET_KEY']
+# Getting environment variables
+consumer_key = os.environ['TW_API_KEY']
+consumer_secret = os.environ['TW_API_SECRET_KEY']
 access_token = os.environ['TW_ACCESS_TOKEN']
 access_token_secret = os.environ['TW_ACCESS_TOKEN_SECRET']
 
@@ -13,6 +12,10 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def search(query):
+    """Wrapper for api.search(); returns list of (possibly)
+    extended tweets (up to 280 characters)"""
+    search_result = api.search(query, tweet_mode='extended')
+    tweets = [tweet.full_text for tweet in search_result]
+    return tweets
