1dcd3dd37c0cb6509aab0f29298445e54b2ebfca
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-03-15 17:43:33 +0100

    Add handling of retweets

--- twst/twitter.py
+++ twst/twitter.py
@@ -13,9 +13,22 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
+def get_full_text(status):
+    if hasattr(status, "retweeted_status"):  # Check if Retweet
+        try:
+            return status.retweeted_status.full_text
+        except AttributeError:
+            return status.retweeted_status.text
+    else:
+        try:
+            return status.full_text
+        except AttributeError:
+            return status.text
+
+
 def search(query):
     """Wrapper for api.search(); returns list of (possibly)
     extended tweets (up to 280 characters)"""
-    search_result = api.search(query, tweet_mode='extended')
-    tweets = [tweet.full_text for tweet in search_result]
+    search_result = api.search(query, tweet_mode='extended', count=100)
+    tweets = [get_full_text(tweet) for tweet in search_result]
     return tweets
