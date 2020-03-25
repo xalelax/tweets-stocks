0ce71e9b0c183ada49001312757191b141f1cf57
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-03-25 22:15:57 +0100

    Feature/get stock data (#2)
    * Update virtual env
    
    * Add minimal working code for getting stocks data

---
+++ twst/stockdata.py
@@ -0,0 +1,9 @@
+from finnhub import client as Finnhub
+from os import environ
+
+client = Finnhub.Client(api_key=environ['FH_API_KEY'])
+
+
+def get_candles(symbol, resolution=60, count=168):
+    response = client.stock_candle(symbol='GOOG', resolution=60, count=168)
+    return response
