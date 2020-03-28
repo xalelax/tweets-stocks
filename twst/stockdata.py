89f8a8a3a32e80eb9597b6a0f64b6e39c6297c3f
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-03-28 15:50:36 +0100

    Feature/allow inputs (#4)
    * Change indentation html template
    
    * Fix wrong invocation of client.stock_candle()
    
    * Add text inputs and buttons to template
    
    * Integrate input fields with js code

--- twst/stockdata.py
+++ twst/stockdata.py
@@ -5,7 +5,7 @@ client = Finnhub.Client(api_key=environ['FH_API_KEY'])
 
 
 def get_candles(symbol, resolution=60, count=168):
-    response = client.stock_candle(symbol,
+    response = client.stock_candle(symbol=symbol,
                                    resolution=resolution,
                                    count=count)
     return response
