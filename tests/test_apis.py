11abcbc8c9598c82e66634ca0db678ac3d1ab469
Alessandro <alessandro.angioi@hotmail.com>
2020-03-29 16:43:15 +0200

    Add integration tests

---
+++ tests/test_apis.py
@@ -0,0 +1,40 @@
+import twst.twitter as t
+import twst.stockdata as s
+import pytest
+from datetime import datetime, timedelta
+
+
+@pytest.fixture(scope='module')
+def vcr_config():
+    return {
+        # Replace the Authorization request header with "DUMMY" in cassettes
+        "filter_headers": [('authorization', 'DUMMY')],
+        "filter_query_parameters": [('token', 'DUMMY')]
+    }
+
+
+@pytest.mark.vcr()
+def test_search():
+    """Check that tweets have expected attributes"""
+    tweets = t.search('physics')
+    attributes = ['created_at', 'author', 'retweet_count', 'text']
+    for att in attributes:
+        for tweet in tweets:
+            assert att in tweet
+    assert type(tweet['retweet_count']) is int
+
+
+@pytest.mark.vcr()
+def test_search2():
+    """Check until argument is working"""
+    threshold = datetime.now() - timedelta(days=3)
+    tweets = t.search('quantum', threshold)
+    for tweet in tweets:
+        assert tweet['created_at'] < threshold
+
+
+@pytest.mark.vcr()
+def test_get_candles():
+    """Check Finnhub data"""
+    data = s.get_candles('AAPL')
+    assert data['s'] == 'ok'
