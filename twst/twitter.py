5ab6d514ead21bedb6eebf6fdd6d305cc5fdc63f
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-03-29 13:02:27 +0200

    Feature/candle and scatter (#6)
    * Join twitter and stock data
    
    * Handle better retrieval of tweets
    
    * Fix bug in default word/symbol
    
    * Draw candles for stocks and scatter for tweets
    
    * Change point style to star
    
    * Change range of candlestick data
    
    * Adjusted legend

--- twst/twitter.py
+++ twst/twitter.py
@@ -14,6 +14,7 @@ api = tweepy.API(auth)
 
 
 def get_full_text(status):
+    """Retrieves full text from extended tweets/retweets"""
     if hasattr(status, "retweeted_status"):  # Check if Retweet
         try:
             return 'RT ' + status.retweeted_status.full_text
@@ -26,10 +27,16 @@ def get_full_text(status):
             return status.text
 
 
-def search(query, result_type='popular'):
+def search(query, until=None, result_type='popular'):
     """Wrapper for api.search(); returns list of (possibly)
-    extended tweets (up to 280 characters)"""
+    extended tweets (up to 280 characters).
+
+    Caveat: until must be a datetime object"""
+
+    fmt = '%Y-%m-%d'
+    until_formatted = until.strftime(fmt)
     search_result = api.search(query + ' -filter:retweets',
+                               until=until_formatted,
                                tweet_mode='extended',
                                result_type=result_type,
                                count=100)
@@ -38,4 +45,24 @@ def search(query, result_type='popular'):
                'retweet_count': tweet.retweet_count,
                'text': get_full_text(tweet)}
               for tweet in search_result]
+    return tweets
+
+
+def get_tweets(query, min_date, max_date):
+    """Twitter's search api returns only 15 results when
+    result_type='popular'. Here, I make as many queries
+    as needed to get historical data"""
+
+    tweets = search(query, until=max_date)
+    if not tweets:
+        return []
+
+    dates = [tweet['created_at'] for tweet in tweets]
+
+    calls = 1
+    while dates and (min(dates) > min_date) and calls < 4:
+        older_tweets = get_tweets(query, min_date, max_date=min(dates))
+        dates = [tweet['created_at'] for tweet in older_tweets]
+        tweets.extend(older_tweets)
+
     return tweets
