783ecbe68b869d91124b734c7a6ba2abb52c8309
Alessandro <alessandro.angioi@hotmail.com>
2020-03-29 09:39:45 +0200

    Handle better retrieval of tweets

--- twst/server.py
+++ twst/server.py
@@ -1,6 +1,7 @@
 import stockdata
 import twitter
 from flask import Flask, render_template, jsonify
+from datetime import datetime
 app = Flask(__name__)
 
 
@@ -11,10 +12,17 @@ def main(name=None):
 
 @app.route('/data/<symbol>/<word>')
 def get_data(symbol, word):  # TODO : refactor this function
+    # Getting data from external APIs
     candles = stockdata.get_candles(str(symbol))
-    tweets = twitter.search(str(word))
-    # Giving back only time and opening_prices
-    stock_data = {'t': candles['t'], 'o': candles['o']}
+    # Converting unix time to datetime for stocks
+    stock_data = candles.copy()
+    stock_data['t'] = [datetime.fromtimestamp(epoch)
+                       for epoch in stock_data['t']]
+
+    # Searching for tweets in overlapping range
+    max_date, min_date = max(stock_data['t']), min(stock_data['t'])
+
+    tweets = twitter.get_tweets(str(word), min_date, max_date)
 
     return jsonify({'stock_data': stock_data,
                     'tweets': tweets})
