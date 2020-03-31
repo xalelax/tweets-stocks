32e3bbbb743b5f8cd8bd4a517ff58b35c8e7d77b
Alessandro <alessandro.angioi@hotmail.com>
2020-03-31 13:34:01 +0200

    Repackage DataTable creations into separate functions

--- twst/static/twst.js
+++ twst/static/twst.js
@@ -9,6 +9,55 @@ function floorDate(datetime) {
     return newDate;
 }
 
+function buildStockDataTable(rawData) {
+
+    var stockDataRaw = rawData.stock_data;
+
+    // Define skeleton of stock DataTable.
+    var stockData = new google.visualization.DataTable();
+    stockData.addColumn('date', 'Date');
+    stockData.addColumn('number', 'Low');
+    stockData.addColumn('number', 'Open');
+    stockData.addColumn('number', 'Close');
+    stockData.addColumn('number', 'High');
+
+    // Populate it
+    for(i = 0; i < stockDataRaw.t.length; i++)
+        stockData.addRow([
+	    floorDate(stockDataRaw.t[i]),
+	    stockDataRaw.l[i],
+	    stockDataRaw.o[i],
+	    stockDataRaw.c[i],
+	    stockDataRaw.h[i]]);
+
+    return stockData;
+}
+
+
+function buildTwitterDataTable(rawData) {
+
+    var tweets = rawData.tweets;
+
+    // Create skeleton of tweets data table
+    var twitterData = new google.visualization.DataTable();
+    twitterData.addColumn('date', 'Date');
+    twitterData.addColumn('number', 'Retweet Count');
+    twitterData.addColumn({type: 'string', role: 'tooltip'});
+    // Populate it
+    for(i = 0; i < tweets.length; i++) {
+	tweet = tweets[i];
+	tweetInfos = '@' + tweet.author + ': ' + tweet.text;
+	twitterData.addRow([
+	    floorDate(tweet.created_at),
+	    tweet.retweet_count,
+	    tweetInfos]);
+    };
+
+    return twitterData;
+
+}
+
+
 function drawChart() {
 
     var symbol = document.getElementById("symbol").value;
@@ -31,39 +80,8 @@ function drawChart() {
 	return
     }
 
-    var stockDataRaw = jsonData.stock_data;
-    var tweets = jsonData.tweets;
-
-    // Create the stock data table.
-    var stockData = new google.visualization.DataTable();
-    stockData.addColumn('date', 'Date');
-    stockData.addColumn('number', 'Low');
-    stockData.addColumn('number', 'Open');
-    stockData.addColumn('number', 'Close');
-    stockData.addColumn('number', 'High');
-
-    for(i = 0; i < stockDataRaw.t.length; i++)
-        stockData.addRow([
-	    floorDate(stockDataRaw.t[i]),
-	    stockDataRaw.l[i],
-	    stockDataRaw.o[i],
-	    stockDataRaw.c[i],
-	    stockDataRaw.h[i]])
-
-    // Create tweets data table
-    var twitterData = new google.visualization.DataTable();
-    twitterData.addColumn('date', 'Date');
-    twitterData.addColumn('number', 'Retweet Count');
-    twitterData.addColumn({type: 'string', role: 'tooltip'});
-
-    for(i = 0; i < tweets.length; i++) {
-	tweet = tweets[i];
-	tweetInfos = '@' + tweet.author + ': ' + tweet.text;
-	twitterData.addRow([
-	    floorDate(tweet.created_at),
-	    tweet.retweet_count,
-	    tweetInfos]);
-    }
+    stockData = buildStockDataTable(jsonData)
+    twitterData = buildTwitterDataTable(jsonData)
 
     twitterData.sort([{column: 0}]);
 
